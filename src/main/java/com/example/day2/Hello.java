f19bbdb6c593db8a73f1c48c778335fe7bc1ee7c
Harish mahur <hmahur20@gmail.com>
2022-09-15 22:49:06 +0530

    modified:using query

--- src/main/java/com/example/day2/Hello.java
+++ src/main/java/com/example/day2/Hello.java
@@ -1,14 +1,24 @@
 package com.example.day2;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hello {
 
+    //UC1_UsingGet
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello From Bridgelabz";
+    }
+
+    //UC2_QueryParametr
+
+    @GetMapping(value="/query")
+    public String hello(@RequestParam (value="name") String name){
+        return "hello" + name;
     }
 }
 
